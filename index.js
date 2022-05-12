d92ae6cb8e64452efecbd30c1124da4eece173c4
yamankubra <ymn.kubra@gmail.com>
2022-05-13 00:04:40 +0300

    hello world

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const app = express();
 const helmet = require("helmet"); 
-const morgan = require("morgan");  
+const morgan = require("morgan");   
 
 app.use(express.json());
 app.use(helmet());
 app.use(morgan("common"));
+
+app.get('/', (req, res) => {
+  res.send('Merhaba Dünya!')
+});
 
 app.listen(5050, () => { 
   console.log("Backend Server is running on port 5050");
