663d81260040cee7aeb1054d2b0d8fe980da618d
yamankubra <ymn.kubra@gmail.com>
2022-05-13 00:13:11 +0300

    created router

--- index.js
+++ index.js
@@ -8,7 +8,35 @@ router.use(helmet());
 router.use(morgan("common"));
 
 router.get('/', (req, res) => {
-  res.send('Merhaba Dünya!')
+  res.send('Hello World!')
+});
+
+router.get('/account', (req, res) => {
+  res.send('Hello Account!')
+});
+
+router.get('/account/:id', (req, res) => {
+  res.send('Hello Account Info!')
+});
+
+router.get('/account/:id', (req, res) => {
+  res.send('Hello Payment!')
+});
+
+router.get('/payment', (req, res) => {
+  res.send('Hello Payment!')
+});
+
+router.get('/deposit', (req, res) => {
+  res.send('Hello Deposit!')
+});
+
+router.get('/withdraw', (req, res) => {
+  res.send('Hello Withdraw!')
+});
+
+router.get('/accounting/:id', (req, res) => {
+  res.send('Hello Transaction History!')
 });
 
 router.listen(5050, () => { 
