f3bd2450ea0182de1e0709adacea606ed8a71621
yamankubra <ymn.kubra@gmail.com>
2022-05-13 00:06:17 +0300

    changed express name

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 const express = require("express");
-const app = express();
+const router = express();
 const helmet = require("helmet"); 
 const morgan = require("morgan");   
 
-app.use(express.json());
-app.use(helmet());
-app.use(morgan("common"));
+router.use(express.json());
+router.use(helmet());
+router.use(morgan("common"));
 
-app.get('/', (req, res) => {
+router.get('/', (req, res) => {
   res.send('Merhaba Dünya!')
 });
 
-app.listen(5050, () => { 
+router.listen(5050, () => { 
   console.log("Backend Server is running on port 5050");
 }); 
